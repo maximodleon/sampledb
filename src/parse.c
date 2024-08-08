0058ba7b919a788dcbe11dee6536589cef5018f0
Maximo De Leon <maximodleon@gmail.com>
2024-08-07 21:48:38 -0400

    Start parsing db header

---
+++ src/parse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+
+#include "common.h"
+#include "parse.h"
+
+
+int create_db_header(int fd, struct dbheader_t **headerOut) {
+}
